4803948a7597968adc078b420c96a42c13102853
Kaur <45065545+bkwomencode@users.noreply.github.com>
2019-09-26 11:55:30 +0100

    Add files via upload

---
+++ ScanBarcode/CircleLibrary/CircleButton.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CircleLibrary
+{
+    public partial class CircleButton: Button
+    {
+        public CircleButton()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnPaint(PaintEventArgs pevent)
+        {
+            GraphicsPath grPath = new GraphicsPath();
+            grPath.AddEllipse(0, 0, ClientSize.Width, ClientSize.Height);
+            this.Region = new System.Drawing.Region(grPath);
+            base.OnPaint(pevent);
+        }
+    }
+}
