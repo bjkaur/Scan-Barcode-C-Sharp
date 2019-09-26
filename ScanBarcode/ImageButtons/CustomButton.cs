b1ba1cd868785d9ad346c46bfa2f01d49f1b5140
Kaur <45065545+bkwomencode@users.noreply.github.com>
2019-09-26 11:56:20 +0100

    Add files via upload

---
+++ ScanBarcode/ImageButtons/CustomButton.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageButtons
+{
+    public partial class CustomButton: PictureBox
+    {
+        public CustomButton()
+        {
+            InitializeComponent();
+        }
+
+        private Image NormalImage;
+        private Image hoverImage;
+
+        private Image Img
+        {
+            get
+            {
+                return NormalImage;
+            }
+            set
+            {
+                NormalImage = value;
+            }
+        }
+
+        private Image ImgHover
+        {
+            get
+            {
+                return hoverImage;
+            }
+            set
+            {
+                hoverImage = value;
+            }
+        }
+
+        private void CustomButton_MouseHover(object sender, EventArgs e)
+        {
+            this.Image = hoverImage;
+        }
+
+        private void CustomButton_MouseLeave(object sender, EventArgs e)
+        {
+            this.Image = NormalImage;
+        }
+    }
+}
